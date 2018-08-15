2e0ca43c6ed8dd761feb1fd5fad6c5cc7deeb529
alexisfeinberg <40009984+alexisfeinberg@users.noreply.github.com>
2018-08-14 21:58:53 -0400

    Delete hangman.py

--- hangman.py
+++
@@ -1,6 +0,0 @@
-class Hangman():
-
-    def __init__(self, word, guesses=0, guessed_letters=[]):
-        self.word = word
-        self.guesses = guesses
-        self.guessed_letters = guessed_letters
